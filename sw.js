45f6499462170dbacb7ba122cc066424f2660015
vlinhares80-creator <vlinhares80@gmail.com>
2026-07-01 15:03:48 -0300

    Add service worker for caching and fetch handling

---
+++ sw.js
@@ -0,0 +1,59 @@
+const CACHE_NAME = "Go-Pam-Pam-v01";
+
+const FILES_TO_CACHE = [
+  "./",
+  "./index.html?v=310",
+  "./style.css?v=310",
+  "./questions.js?v=310",
+  "./app.js?v=310",
+  "./manifest.json?v=310",
+  "./icon.svg"
+];
+
+self.addEventListener("install", (event) => {
+  self.skipWaiting();
+
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(FILES_TO_CACHE).catch(() => {
+        return Promise.resolve();
+      });
+    })
+  );
+});
+
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cacheName) => {
+          if (cacheName !== CACHE_NAME) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    }).then(() => {
+      return self.clients.claim();
+    })
+  );
+});
+
+self.addEventListener("fetch", (event) => {
+  if (event.request.method !== "GET") return;
+
+  event.respondWith(
+    fetch(event.request)
+      .then((response) => {
+        const responseClone = response.clone();
+
+        caches.open(CACHE_NAME).then((cache) => {
+          cache.put(event.request, responseClone);
+        });
+
+        return response;
+      })
+      .catch(() => {
+        return caches.match(event.request);
+      })
+  );
+});
